b8c5b4c2bac078accb779c82523349317802f4f0
EsraaSaad298 <esraa.abdo@student.guc.edu.eg>
2021-08-23 22:26:35 +0400

    changes

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Picker, Image, FlatList } from 'react-native';
-
+import { StyleSheet, Text, View, Picker, Image, FlatList, ScrollView } from 'react-native';
 
 export default function App() {
   const [selectedValue, setSelectedValue] = useState("all");
@@ -11,23 +10,30 @@ export default function App() {
       <Text style={styles.headline}> Choose Cusine: </Text>
       <Picker
         selectedValue={selectedValue}
-        style={{ height: 650, width: 150 }}
+        style={styles.picker}
         onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
         >
         <Picker.Item label="All" value="all" />
-        <Picker.Item label="Fast Food" value="fastFood" />
+        <Picker.Item label="Sandwiches" value="sandwiches" />
         <Picker.Item label="Grilled" value="grilled" />
-        <Picker.Item label="Pasta" value="pasta" />
+        <Picker.Item label="Pizza" value="pizza" />
       </Picker>
-      {selectedValue == 'pasta'?<Image source={require('./Pasta.png')} style={styles.img}/>: null }
-      {selectedValue == 'grilled'?<Image source={require('./Grilled.jpg')} style={styles.img} />: null }
-      {selectedValue == 'fastFood'?<Image source={require('./Mcdonalds.png')} style={styles.img} />: null }
-      {selectedValue == 'all'? <Image source={require('./all.png')} style={styles.imgb} /> : null }
 
-      {/* <Image source={require('./Mcdonalds.png')} style={styles.imgl} />
-      <Image source={require('./Pasta.png')} style={styles.imgr} />
-      <Image source={require('./Grilled.jpg')} style={styles.imgb} /> */}
+      {selectedValue == 'pizza'?<ScrollView>
+    <Image source={require('./pizza.png')} />
+  </ScrollView>: null }
 
+      {selectedValue == 'sandwiches'?<ScrollView>
+    <Image source={require('./sandwiches.png')} />
+  </ScrollView>: null }
+
+      {selectedValue == 'grilled'? <ScrollView>
+    <Image source={require('./grilled.png')} />
+  </ScrollView>: null }
+
+      {selectedValue == 'all'? <ScrollView>
+    <Image source={require('./thumbnail.png')} />
+  </ScrollView>: null }
     </View>
   );
 }
@@ -43,17 +49,12 @@ const styles = StyleSheet.create({
     textAlign: 'center', 
     fontWeight: 'bold',
     fontSize: 20,
-    marginTop: 0,
+    marginTop: 30,
     width: 200,
   },
-  img: {
-    width: 200,
-    position: 'absolute',
-    height: 200,
-  },
-  imgb: {
-    width: 300,
-    position: 'absolute',
-    height: 300,
+  picker: {
+    height: 200, 
+    width: 150,
+    marginTop: -70,
   },
 });
